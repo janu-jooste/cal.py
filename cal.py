f4921966bf2ac854bafcf55d7f862c06e5d96271
Janu Jooste <janl.jooste@gmail.com>
2025-10-26 09:58:06 +0200

    more advanced but stil basic

--- cal.py
+++ cal.py
@@ -1,24 +1,38 @@
 sum = (input("Enter calculation: "))
 opp = ['+', '-', '*', '/']
 
-if '+' in sum:
-    numbers = sum.split('+')
-    result = float(numbers[0]) + float(numbers[1])
-    print("Result:", result)
-elif '-' in sum:
-    numbers = sum.split('-')
-    result = float(numbers[0]) - float(numbers[1])
-    print("Result:", result)
-elif '*' in sum:
-    numbers = sum.split('*')
-    result = float(numbers[0]) * float(numbers[1])
-    print("Result:", result)
-elif '/' in sum:            
-    numbers = sum.split('/')
-    if float(numbers[1]) == 0:
-        print("Error: Division by zero is not allowed.")
-    else:
-        result = float(numbers[0]) / float(numbers[1])
+for opp in sum:
+    numbers = sum.split(opp)
+    if opp == '+':
+        result = float(numbers[0]) + float(numbers[1])
         print("Result:", result)
-else:
-    print("Error: Invalid operation. Please use +, -, *, or /.")
+    elif opp == '-':
+        result = float(numbers[0]) - float(numbers[1])
+        print("Result:", result)
+    elif opp == '*':
+        result = float(numbers[0]) * float(numbers[1])
+        print("Result:", result)
+    elif opp == '/':          
+        if float(numbers[1]) == 0:
+            print("Error: Division by zero is not allowed.")
+        else:
+            result = float(numbers[0]) / float(numbers[1])
+            print("Result:", result)
+    elif opp == '%':
+        result = float(numbers[0]) % float(numbers[1])
+        print("Result:", result)
+    elif opp == '**':
+        result = float(numbers[0]) ** float(numbers[1])
+        print("Result:", result)
+    elif opp == '<':
+        result = float(numbers[0]) < float(numbers[1])
+        if result is True :
+            print("Result: True")
+        else :
+            print("Result: False")
+    elif opp == '>':
+        result = float(numbers[0]) > float(numbers[1])
+        if result is True :
+            print("Result: True")
+        else :
+            print("Result: False")
